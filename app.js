95847d8f927b55f771389c1563c66db0925baa21
Yous16 <128783933+Yous16@users.noreply.github.com>
2023-12-15 11:17:47 +0100

    Edited twitter link

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ const generateQuote = () => {
   let randomIndex = Math.floor(Math.random() * quoteSize);
   let randomQuoteData = QUOTES[randomIndex];
 
-  let twitterLink = "https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amp;text=%22"
+  let twitterLink =
+    "https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amp;text=%22";
 
   let quoteInApiFormat = randomQuoteData.quote.replace(/ /g, "%20");
   twitterLink += quoteInApiFormat;
@@ -42,4 +43,3 @@ const generateQuote = () => {
   document.getElementById("text").innerText = randomQuoteData.quote;
   document.getElementById("author").innerText = randomQuoteData.author;
 };
-
